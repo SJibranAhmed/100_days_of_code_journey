0631668a2aa2a0e053e909ba969b028a6168e524
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-03-07 09:38:08 +0500

    Names,Adding_comment & greetings

---
+++ day_4/greetings.ts
@@ -0,0 +1,12 @@
+// Sheikh Jibran Ahmed  2024 / march / 03
+
+//list of elements is called Array.Its index is start from 0 to 1,2,3.....
+//we can also access all element of array by forEach() method
+
+let friendsName:string[]= ['Munaaf', 'Talha', 'Hasan', 'Waiz']
+
+//access array elements one by one by forEach() method of array
+friendsName.forEach((elemOfArr)=>{
+    console.log('Assalaamualaikum ! \n' + elemOfArr + ' how are you?' )
+})
+
---
+++ day_4/names.js
@@ -0,0 +1,10 @@
+//Sheikh Jibran Ahmed   2024/ march /03
+//Names in an array
+//list of elements is called Array.Its index is start from 0 to 1,2,3.....
+//store names in an array
+var friendsName = ['Munaaf', 'Talha', 'Hasan', 'Waiz'];
+//access array elements one by one with index value
+console.log('value of index 0 is : ' + friendsName[0]);
+console.log('value of index 1 is : ' + friendsName[1]);
+console.log('value of index 2 is : ' + friendsName[2]);
+console.log('value of index 3 is : ' + friendsName[3]);
